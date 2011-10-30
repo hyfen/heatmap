1cbd5d06bd1bab6d390dccddbd1dab37fd752010
Glenn Gillen <me@glenngillen.com>
2011-10-30 16:09:27 +0000

    Create from lat and long.

--- lib/heatmap/point.rb
+++ lib/heatmap/point.rb
@@ -1,16 +1,22 @@
 module Heatmap
 
   class Point
-    
+
     DEFAULT_INTENSITY = 1.0
-  
+
     attr_accessor :x, :y, :intensity
-  
+
     def initialize(x, y, intensity=DEFAULT_INTENSITY)
       @x = x
       @y = y
       @intensity = intensity
     end
+
+    def self.from_lat_lng(lat, lng, intensity)
+      x = Heatmap::Mercator.lng_to_x(lng)
+      y = Heatmap::Mercator.lat_to_y(lat)
+      new(x, y, intensity)
+    end
   end
-  
+
 end
